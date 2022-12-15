04af45cbb5174d292b2e1e7f5ed80a1d0e9ae890
Abhishek Chandra Jha <58040436+Abhishek18jha@users.noreply.github.com>
2022-12-15 05:06:04 +0000

    exotic

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ with open('url_file.txt', 'w') as f:
     print(link.get('href'))
     f.write(link.get('href'))
     f.write("\n")
+
+
+# Exotic India Main Page All URLs
+
+url_exotic ="https://www.exoticindiaart.com/"
+
+html_exotic = requests.get(url)
+html_exotic = html_exotic.text
+
+soup_exotic = BeautifulSoup(html_exotic, 'html.parser')
+# print(soup.prettify)
+# print(soup.title.string +" : "+soup.title.name)
+# print(soup.find_all('a')[0])
+with open('exotic_url_file.txt', 'w') as f:
+  for link in soup_exotic.find_all('a'):
+    print(link.get('href'))
+    f.write(link.get('href'))
+    f.write("\n")
