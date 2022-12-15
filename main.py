39a862956bb7af67da3b262e643d374d548268fb
Abhishek Chandra Jha <58040436+Abhishek18jha@users.noreply.github.com>
2022-12-15 05:44:16 +0000

    data

--- main.py
+++ main.py
@@ -11,24 +11,47 @@ soup = BeautifulSoup(html, 'html.parser')
 # print(soup.find_all('a')[0])
 with open('url_file.txt', 'w') as f:
   for link in soup.find_all('a'):
-    print(link.get('href'))
+    # print(link.get('href'))
     f.write(link.get('href'))
     f.write("\n")
 
-
 # Exotic India Main Page All URLs
 
-url_exotic ="https://www.exoticindiaart.com/"
+url_exotic ="https://www.exoticindiaart.com"
 
 html_exotic = requests.get(url_exotic)
 html_exotic = html_exotic.text
-
+list_of_link =[]
 soup_exotic = BeautifulSoup(html_exotic, 'html.parser')
 # print(soup.prettify)
 # print(soup.title.string +" : "+soup.title.name)
 # print(soup.find_all('a')[0])
 with open('exotic_url_file.txt', 'w') as f:
   for link in soup_exotic.find_all('a'):
-    print(link.get('href'))
+    # print(link.get('href'))
     f.write(link.get('href'))
+    list_of_link.append(link.get('href'))
     f.write("\n")
+
+use_url=[]
+for i in list_of_link[25:50]:
+  print(url_exotic+i)
+  use_url.append(url_exotic+i)
+# for i in use_url[0:3]:
+#   print(use_url[0:3])
+
+html_exotic = requests.get(url_exotic)
+html_exotic = html_exotic.text
+list_of_link =[]
+soup_exotic = BeautifulSoup(html_exotic, 'html.parser')
+with open('exotic_url_file.txt', 'w') as f:
+  for i in use_url:
+    html_exotic = requests.get(i)
+    html_exotic = html_exotic.text
+    list_of_link =[]
+    soup_exotic = BeautifulSoup(html_exotic, 'html.parser')
+    for link in soup_exotic.find_all('a'):
+      # print(link.get('href'))
+      f.write(link.get('href'))
+      list_of_link.append(link.get('href'))
+      f.write("\n")
