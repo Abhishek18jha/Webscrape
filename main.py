225c3bd3aeb1ac3c68d37dd006879c557921ac4e
Abhishek Chandra Jha <58040436+Abhishek18jha@users.noreply.github.com>
2022-12-15 05:01:25 +0000

    new line appeared

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from bs4 import BeautifulSoup
-import requests 
-url="https://quotes.toscrape.com/"
+import requests
+
+url = "https://quotes.toscrape.com/"
 html = requests.get(url)
-html =html.text
+html = html.text
 
 soup = BeautifulSoup(html, 'html.parser')
 # print(soup.prettify)
 # print(soup.title.string +" : "+soup.title.name)
 # print(soup.find_all('a')[0])
-with open('url_file.txt','w') as f:
+with open('url_file.txt', 'w') as f:
   for link in soup.find_all('a'):
     print(link.get('href'))
     f.write(link.get('href'))
+    f.write("\n")
